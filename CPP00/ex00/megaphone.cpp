93128707052870897ca1fd45e114b64ad749a0da
Astrid GAULTIER <asgaulti@e3r8p17.clusters.42paris.fr>
2021-12-10 11:31:47 +0100

    c00 ex00 makefile 1

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,9 +6,15 @@
 /*   By: asgaulti <asgaulti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:02:26 by asgaulti          #+#    #+#             */
-/*   Updated: 2021/12/10 11:03:08 by asgaulti         ###   ########.fr       */
+/*   Updated: 2021/12/10 11:25:10 by asgaulti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+using namespace std;
 
+int main()
+{
+    cout << "Hello world" << endl; // initialiser cout (mais pas avec using namespace)
+    return (0);
+}
