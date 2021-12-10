a59305a498c918e96907bb9e7a39af2e4f4d5585
Astrid GAULTIER <asgaulti@e2r3p12.clusters.42paris.fr>
2021-12-10 15:30:55 +0100

    c00 ex00 ok

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,15 +6,27 @@
 /*   By: asgaulti <asgaulti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 11:02:26 by asgaulti          #+#    #+#             */
-/*   Updated: 2021/12/10 11:25:10 by asgaulti         ###   ########.fr       */
+/*   Updated: 2021/12/10 15:30:29 by asgaulti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-using namespace std;
+#include <iostream> //std::cout
+#include <string> //std::string
 
-int main()
+int	main(int ac, char **av)
 {
-    cout << "Hello world" << endl; // initialiser cout (mais pas avec using namespace)
-    return (0);
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+	{
+		std::string str(av[i]);
+		for (size_t j = 0; j < str.length();  j++)
+			std::cout << (char)toupper(str[j]); // caster le retour de toupper pour l'avoir en char
+		if (i != ac - 1)
+			std::cout << ' ';
+	}
+	std::cout << std::endl;
+	return (0);
 }
+
+// a verifier : toupper de C autorise (ou celui de c++)???
