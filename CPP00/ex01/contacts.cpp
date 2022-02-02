6ce69cc74a804791b26fb0bf3dded7e865024c29
Astrid Gaultier <astridgaultier@gmail.com>
2022-02-02 15:23:16 +0100

    add contact 4

--- CPP00/ex01/contacts.cpp
+++ CPP00/ex01/contacts.cpp
@@ -6,7 +6,7 @@
 /*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 13:36:02 by asgaulti          #+#    #+#             */
-/*   Updated: 2022/02/02 12:43:21 by astridgault      ###   ########.fr       */
+/*   Updated: 2022/02/02 15:20:42 by astridgault      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "contacts.hpp"
 
 Contacts::Contacts(){
-	_index = 0;
+	this->_index = 0;
 	//std::cout << _index << _phone_number << std::endl;
 }
 
@@ -28,8 +28,8 @@ void	Contacts::set_contact(int count){
 //	char	*nn;
 //	char	*pn;
 //	char	*ds;
-
 	this->_index = count;
+std::cout << "index= " << _index << std::endl;
 	std::cout << "first name : ";
 	std::cin >> _first_name;
 	std::cout << "last name : ";
@@ -46,34 +46,9 @@ void	Contacts::set_contact(int count){
 	// if phone_number != 0 à 9  et si plus de 10 chiffres> erreur, réécrire?
 	std::cout << "darkest secret : ";
 	std::cin >> _darkest_secret;
-	/*
-	i = _index;
-	i++;
-	std::cout << "first name : ";
-	std::cin >> fn;
-	std::cout << "last name : ";
-	std::cin >> ln;
-	std::cout << " nickname : ";
-	std::cin >> nn;
-	std::cout << "phone number : ";
-	std::cin >> _phone_number;
-	while (_phone_number.size() > 10){
-		std::cout << "error : please use less than 10 characters" << std::endl;
-		std::cout << "phone number : ";
-		std::cin >> pn;
-	}
-	// if phone_number != 0 à 9  et si plus de 10 chiffres> erreur, réécrire?
-	std::cout << "darkest secret : ";
-	std::cin >> ds;
-	this->_index = i;
-	this->_first_name = fn;
-	this->_last_name = ln;
-	this->_nickname = nn;
-	this->_phone_number = pn;
-	this->_darkest_secret = ds;
-	this->_index++;*/
+	//std::cout << this->_index << this->_first_name << _last_name << _nickname << _phone_number << _darkest_secret << std::endl;
 }
 
 void	Contacts::get_contact(void) const{
-	std::cout << _index << _first_name << _last_name << _nickname << _phone_number << _darkest_secret << std::endl;
+	std::cout << this->_index << this->_first_name << _last_name << _nickname << _phone_number << _darkest_secret << std::endl;
 }
--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -6,14 +6,14 @@
 /*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:04:48 by asgaulti          #+#    #+#             */
-/*   Updated: 2022/02/02 12:42:39 by astridgault      ###   ########.fr       */
+/*   Updated: 2022/02/02 15:22:52 by astridgault      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
 Phonebook::Phonebook(){
-	this->count = 1; // initialiser count à 0 pour l'instance courante
+	this->count = 0; // initialiser count à 0 pour l'instance courante
     //this->xxx()  > appellera la fct xxx dans l'instance courante	
     return ;
 }
@@ -23,12 +23,11 @@ Phonebook::~Phonebook(){
 
 void	Phonebook::add(void)
 {
-    Contacts    contacts;
-
 	if (count >= 8){
 		std::cout << "There is no more place" << std::endl;
 	}
-    this->contacts[this->count + 1].set_contact(this->count + 1);
-    //contacts.get_contact();
+    this->contacts[this->count].set_contact(this->count);
+    //contacts[this->count].get_contact();
 	this->count++;
+    //std::cout << count << std::endl;
 }
