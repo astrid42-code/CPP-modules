49f34fd55e347ea624695d636d449363d5966c03
Astrid Gaultier <astridgaultier@gmail.com>
2022-02-05 11:38:33 +0100

    ex02-2

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 10:13:52 by astridgault       #+#    #+#             */
+/*   Updated: 2022/02/05 10:43:35 by astridgault      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "account.h"
+
+Account::Account(){
+}
+
+Account::Account(int initial_deposit){
+    
+}
+
+Account::~Account(void){
+
+}
