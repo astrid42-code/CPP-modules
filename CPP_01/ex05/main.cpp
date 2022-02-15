a9a0bffb4211d721b38253004bb5bfd844733849
Astrid GAULTIER <asgaulti@dump-tuteur.clusters.42paris.fr>
2022-02-15 14:40:03 +0100

    cpp01-ex5 main

--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
+/*   By: asgaulti <asgaulti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 17:07:29 by astridgault       #+#    #+#             */
-/*   Updated: 2022/02/12 19:54:51 by astridgault      ###   ########.fr       */
+/*   Updated: 2022/02/15 14:39:33 by asgaulti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 
 int main(int ac, char **av){
 	Karen		karen;
+	std::string	complaint;
 	
-	karen.complain("debug");
-	karen.complain("warning");
-	karen.complain("info");
-	karen.complain("error");
-	karen.complain("prout");
-	karen.complain("");
+	while (1){
+		if (std::cin.eof())
+        {
+           	std::cout << "Goodbye !" << std::endl;
+           	exit(0);
+       	}
+		   std::cout << "Hi Karen, what is your complaint?" << std::endl;
+		std::getline(std::cin, complaint);
+		karen.complain(complaint);
+	}
 	return (0);
 }
+
+// 4 complaintes acceptees : debug, warning, info, error
+// sinon msg d'erreur
