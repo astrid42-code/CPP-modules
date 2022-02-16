b91f9923f08d0231bebb83e59ecb217265891f75
Astrid Gaultier <astridgaultier@gmail.com>
2022-02-16 21:57:12 +0100

    cpp02-ex02 +-*/ 2

--- CPP_02/ex02/main.cpp
+++ CPP_02/ex02/main.cpp
@@ -6,16 +6,17 @@
 /*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 16:27:57 by asgaulti          #+#    #+#             */
-/*   Updated: 2022/02/16 19:51:23 by astridgault      ###   ########.fr       */
+/*   Updated: 2022/02/16 21:56:53 by astridgault      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main( void ) {
-	Fixed a(3); // quelle est la valeur de a? (constructeur par défaut non?)
+	Fixed a; //(constructeur par défaut, a = 0)
 //	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	Fixed const b(5);
+	Fixed const b(Fixed(5.05f));
+//	Fixed const c(a);
 	
 	std::cout << a << std::endl;
 /*	std::cout << ++a << std::endl;
@@ -23,25 +24,37 @@ int main( void ) {
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 */	std::cout << b << std::endl;
+//	std::cout << c << std::endl;
+
+	a = b;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
 
 	a += b;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+//	std::cout << c << std::endl;
 	
 	a -= b;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+//	std::cout << c << std::endl;
 	
 	a *= b;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+//	std::cout << c << std::endl;
 	
 	a /= b;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+//	std::cout << c << std::endl;
 	
 /*	std::cout << Fixed::min( a, b ) << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
 */	
 	return (0);
 }
+
+// pb1: pour chaque copy assignment operator il n'y a pas de destructor called
+// pb2: résultat * chelou
