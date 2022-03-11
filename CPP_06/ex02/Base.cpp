92c3135353e3a664b726adcaf0dcd2a37c48ed88
Astrid Gaultier <astridgaultier@gmail.com>
2022-03-11 18:26:38 +0100

    cpp06-02 init

---
+++ CPP_06/ex02/Base.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 18:25:19 by astridgault       #+#    #+#             */
+/*   Updated: 2022/03/11 18:26:23 by astridgault      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+
+Base::Base(){
+	
+}
+
+Base::Base(const Base &base_cp){
+	
+}
+Base::~Base(){
+	
+}
+
+Base & Base::operator=(const Base &base_op){
+	
+}
+
+void	Base::identify(Base* p){
+	
+}
+
+Base::void	identify(Base& p){
+	
+}
