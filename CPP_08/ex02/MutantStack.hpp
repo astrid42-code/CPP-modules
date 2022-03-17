a4f57f81edb83689cba8565f09779320ddd97259
Astrid Gaultier <astridgaultier@gmail.com>
2022-03-17 15:12:52 +0100

    cpp08-ex02 .hpp ok

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -3,18 +3,47 @@
 
 #include <iostream>
 #include <iomanip>
+#include <stack>
+#include <iterator>
 
-class MutantStack{
+template<typename T>
+class MutantStack : public std::stack<T>{ // fait hériter ma class de la classe std::stack de type T
 	public:
-		MutantStack();
-		MutantStack(const MutantStack & mut_cp);
-		~MutantStack();
+		MutantStack(){}
+		MutantStack(const MutantStack & mut_cp) : std::stack<T>(mut_cp){}
+		~MutantStack(){
+}
 
 		MutantStack & operator=(const MutantStack & mut_op);
-		
+
+		typedef typename std::stack<T>::container_type::iterator iter; // récupérer la classe iterator (++it) pour un type de container
+		typedef typename std::stack<T>::container_type::reverse_iterator rev_iter;
+
+		iter	begin(){
+			//return (this->c.begin());
+			return (std::begin(this->c));
+			// c est un attribut protégé (un container) de la class stack
+			// la fct retourne la valeur de fin de mon container après ++it
+			// attention : nécessité du this-> ici car c est extérieur à la class Mutant
+		}
+
+		iter	end(){
+			//return (this->c.end());
+			return (std::end(this->c));
+		}
+
+		rev_iter	rbegin(){
+			return (this->c.rbegin());
+		}
+
+		rev_iter	rend(){
+			return (this->c.rend());
+		}
 
 	private:
 
 };
 
 #endif
+
+// https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: astridgaultier <astridgaultier@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 21:03:26 by astridgault       #+#    #+#             */
-/*   Updated: 2022/03/16 21:07:39 by astridgault      ###   ########.fr       */
+/*   Updated: 2022/03/17 14:58:42 by astridgault      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,43 @@
 // exemple de main (à augmenter en changeant stack par list et autre trucs)
 int main()
 {
+
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	std::cout << "Top : " << mstack.top() << std::endl;
+	std::cout << "Size : " << mstack.size() << std::endl;
+	
+	MutantStack<int>::iter it = mstack.begin();
+	MutantStack<int>::iter ite = mstack.end();
+	
+	std::cout << "iterator begin-end : " << std::endl;
 	++it;
 	--it;
+	//--ite;
+
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
+	}
+	MutantStack<int>::rev_iter rit = mstack.rbegin();
+	MutantStack<int>::rev_iter rite = mstack.rend();
+	
+	std::cout << "reverse iterator begin-end : " << std::endl;
+	++rit;
+	--rit;
+	--rite;
+
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
 	}
 	std::stack<int> s(mstack);
 	return 0;
